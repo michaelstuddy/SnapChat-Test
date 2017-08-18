855ec9c702ccb64ce2180a703409f0b05c63db99
Michael Ruddy <mrud2001@outlook.com>
2017-08-17 18:15:45 -0700

    Initial Commit

---
+++ SnapchatTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SnapchatTest
+//
+//  Created by Michael Ruddy on 8/17/17.
+//  Copyright © 2017 Michael Ruddy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
